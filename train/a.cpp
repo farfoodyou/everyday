370f3e6e4277db6cf5046700d550e1012c160a4f
farfoodyou <farfoodyou@gmail.com>
2017-05-12 00:00:21 +0800

    2017-05-11

---
+++ train/a.cpp
@@ -0,0 +1,24 @@
+#include <cstido>
+#include <cstring>
+#include <iostream>
+
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 1e5;
+const int INF = 0x3f3f3f3f;
+int N,M,T;
+
+
+int main(){
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+	scanf("%d",&N);
+	for(int i = 0; i < N; i++){
+		
+	}
+	return 0;
+}
+
+

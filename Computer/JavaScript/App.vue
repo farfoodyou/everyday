4722f2c3500ea8f81fc9bde5baa12781beb099d7
farfoodyou <farfoodyou@gmail.com>
2018-08-14 15:55:13 +0800

    JavaScript grammer

---
+++ Computer/JavaScript/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="app">
+		<h1>My Todo App!</h1>
+		<hello/>
+	</div>
+</template>
+
+<script>
+import hello from './hello.vue'
+
+export default {
+	components: {
+		hello
+	}
+}
+</script>
---
+++ Computer/Lua/HelloWorld.lua
@@ -0,0 +1,9 @@
+#!/usr/local/bin/lua
+
+times = 3
+while( times > 0 )
+do
+    times = times - 1;
+    print("循环将执行下去" .. times);
+end
+

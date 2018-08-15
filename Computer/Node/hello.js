7735f6cd218b651ec9f9145ebfe50d21c226f4e6
huyalv <huyalv@baidu.com>
2018-08-15 13:45:53 +0800

    add node

---
+++ Computer/Node/hello.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log('Hello, world.');
+
+var fs = require('fs');
+
+fs.readFile('test.txt', 'utf-8', function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+console.log("=================");
+var data = fs.readFileSync('test.txt', 'utf-8');
+console.log(data + 'sync');

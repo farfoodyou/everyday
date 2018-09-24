c25ce9083324dce5c5d45a4193d7eb8a1c2f238f
huyalv <huyalv@baidu.com>
2018-09-24 14:40:38 +0800

    9-24

--- Computer/Language/PHP/log.php
+++ Computer/Language/PHP/log.php
@@ -5,17 +5,18 @@ header("Content-type: text/html; charset=utf-8");
 2、打开并读取文件内容
 ********************/
 $time = date('Y-m-d H:i:s'); 
+$filename = "./log/".date("Y-m-d").".log";
  $file  = 'log.txt';//要写入文件的文件名（可以是任意文件名），如果文件不存在，将会创建一个
  $content = "${time} Firset Line\n";
 
- if($f  = file_put_contents($file, $content,FILE_APPEND)){// 这个函数支持版本(PHP 5) 
+ if($f  = file_put_contents($filename, $content,FILE_APPEND)){// 这个函数支持版本(PHP 5) 
   echo "写入成功。<br />";
  }
  $content = "${time} 第二次写入的内容\n";
- if($f  = file_put_contents($file, $content,FILE_APPEND)){// 这个函数支持版本(PHP 5)
+ if($f  = file_put_contents($filename, $content,FILE_APPEND)){// 这个函数支持版本(PHP 5)
      echo "写入成功。<br />";
  }
- if($data = file_get_contents($file)){; // 这个函数支持版本(PHP 4 >= 4.3.0, PHP 5) 
+ if($data = file_get_contents($filename)){; // 这个函数支持版本(PHP 4 >= 4.3.0, PHP 5) 
   echo "写入文件的内容是：$data";
  }
 

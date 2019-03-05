1b9011d40b0d41d9b7b400ae82eab647ca527bab
farfoodyou <farfoodyou@gmail.com>
2019-03-05 13:13:30 +0800

    LeetCode 193

---
+++ Computer/LeetCode/LC193.sh
@@ -0,0 +1,9 @@
+grep -P  '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' file.txt 
+
+# Assume that file.txt has the following content:
+# 987-123-4567
+# 123 456 7890
+# (123) 456-7890
+# Your script should output the following valid phone numbers:
+# 987-123-4567
+# (123) 456-7890

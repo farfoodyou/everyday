007da975d31e21202b318dda446ddd3ab4795678
farfoodyou <farfoodyou@gmail.com>
2019-03-20 20:56:58 +0800

    03-20 好难啊, 别人都结婚了, 我还一个人孤苦伶仃, 背井离乡的

--- Computer/Language/C++/template/test.cc
+++ Computer/Language/C++/template/test.cc
@@ -6,14 +6,42 @@ void printValue(T& o){
 }
 
 class Base {
+protected:
     int value;
 public:
     int Value(){
+        std::cout << " Base class Value " << std::endl;
         return value;
     }
     int SetValue(int v){
         value = v;
         return 0;
+    }
+    virtual void print(){
+        std::cout << " base class" << std::endl;
+    }
+};
+
+class A :public Base {
+public:
+    A(){
+        value = 2;
+    }
+    int Value(){
+        std::cout << " A class Balue " << std::endl;
+        return value/2;
+    }
+    int Value2(){
+        return value * 2;
+    }
+    virtual void print(){
+        std::cout << " A class " << std::endl;
+    }
+};
+class B:public Base {
+public:
+    virtual void print(){
+        std::cout << " B class " << std::endl;
     }
 };
 
@@ -22,5 +50,24 @@ int main(){
     printValue(o);
     o.SetValue(1234);
     printValue(o);
+    A a;
+    printValue(a);
+    Base &ao = a;
+    a.print();
+    ao.print();
+    // 下面的结果不同
+    printValue(ao);
+    printValue(a);
+
+    B b;
+    printValue(b);
+    Base &bo = b;
+    b.print();
+    bo.print();
     return 0;
 }
+
+/*
+用模板的时候, 如果重载了, 子类用的是子类的函数, 基类用的是基类的函数. 
+如果用的是虚函数, 那就能够调用子类的函数.
+*/

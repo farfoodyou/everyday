a209f98b1364db1ff0c369dd5d49bf5e85a4c69d
farfoodyou <farfoodyou@gmail.com>
2019-04-07 17:20:40 +0800

    default commit infomation at 2019年 4月 7日 星期日 17时20分40秒 CST

--- push.sh
+++ push.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# This script is tool can add project, commit stage, push project.
+# start of script
 
 git add ./
 date_str=`date`
-git commit -m "$date_str"
+if [ $# == 1 ];then
+    git commit -m "$1"
+else
+    git commit -m "default commit infomation at $date_str"
+fi
+
 git push
+
+# end of script

d4e6dc8f0648a2125d2516016da0aa3f5f0af108
farfoodyou <farfoodyou@gmail.com>
2019-04-12 02:42:32 +0800

    基础 at 2019年 4月12日 星期五 02时42分32秒 CST

---
+++ Computer/DS/base/ADTList.cpp
@@ -0,0 +1,116 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+typedef struct Value{
+    int v;
+}Value;
+
+typedef struct Node{
+    struct Node *next;
+    struct Value v;
+}Node;
+
+typedef struct ADTList{
+    Node *root;
+    void InitList(){
+        root = (Node *)malloc(sizeof(Node));
+        root->next=NULL;
+        root->v.v = 0;
+        printf("init over\n");
+    }
+    void DestroyList(){
+        int len = 0;
+
+        // 初始化有关的知识, 初始化不是在函数调用的时候才初始化的么?
+        // Node *p,*q = root;  我怎么能写出这么蠢的写法
+        Node *p,*q;
+        p = q = root;
+        printf("====[ Destroy ]====\n");
+        printf("%p\n",p);
+        printf("%p\n",root);
+        root = NULL;
+        while(p->next != NULL){
+            printf("%d\n",len);
+            
+            len++;
+            q = p->next; 
+            printf("%d\n",p->v.v);
+            free(p);
+            p = q;
+        }
+    }
+    int ListLength(){
+        int len = -1;
+        Node *p = root;
+        while(p != NULL){
+            len++;
+            p = p->next;
+        }
+        return len;
+    }
+    Node* Head(){
+        return root;
+    }
+    Node* Tail(){
+        Node *p = root;
+        while(p->next != NULL){
+            p = p->next;
+        }
+        return p;
+    }
+    void PrintList(){
+        Node *p = root;
+        while(p->next != NULL){
+            printf("%d\n",p->v.v);
+            printf("%p\n",p);
+            p = p->next;
+        }
+        printf("%p\n",p);
+        printf("%d\n",p->v.v);
+    }
+    int IsEmpty(){
+        Node* t = Tail();
+        if (root != t) {
+            return 1;
+        }
+        return 0;
+    }
+    void ListInsert(Value v){
+        Node *p = Tail();
+        p->next = (Node *)malloc(sizeof(Node));
+        p = p->next;
+        (*p).v = v;
+        p->next = NULL;
+        printf("insert %d\n",v.v);
+    }
+    void hi();
+}List;
+void List::hi(){
+    printf("hi, this is a struct list\n");
+}
+int main(){
+    List list;
+    list.hi();
+    list.InitList();
+    Value v;
+    v.v = 123;
+    list.ListInsert(v);
+    v.v = 33;
+    list.ListInsert(v);
+    Node *t = list.Tail();
+    std::cout << list.ListLength() << std::endl;
+    std::cout << "node tail"<< std::endl;
+
+
+    v.v = 567;
+    list.ListInsert(v);
+    t = list.Tail();
+    std::cout << "node tail: " << t->v.v << std::endl;
+    list.PrintList();
+
+    //std::cout << t->v.v << std::endl;
+    list.DestroyList();
+
+    return 0;
+}
